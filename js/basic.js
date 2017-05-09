b36dfae5ad7b69aeab2af0037c714d37c6afe076
ropere0622 <ropere0622@gmail.com>
2017-05-09 19:09:40 +0800

    Revert "conan"
    This reverts commit 2de559f9938da2252ec70d7dc454ad706f5947a7.

--- js/basic.js
+++
@@ -1,7 +0,0 @@
-
-function tbtn1(){ location.href='news.html';}
-function tbtn2(){ location.href='intro.html';}
-function tbtn3(){ location.href='character.html';}
-function tbtn4(){ location.href='license.html';}
-function tbtn5(){ location.href='broadcast.html';} 
-function tbtn6(){ location.href='contact.html';}
